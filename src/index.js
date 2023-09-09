e88a60a5b768c963d36aea8c7355a8fd63e1eabc
Parchello <parchellos@gmail.com>
2023-09-09 12:23:28 +0300

    fixed page

--- src/index.js
+++ src/index.js
@@ -20,6 +20,23 @@ const lightbox = new SimpleLightbox('.gallery a', {
 elements.form.addEventListener('submit', onSubmit);
 elements.btnLoad.addEventListener('click', downloadMore);
 
+function onSubmit(evt) {
+  evt.preventDefault();
+  page = 1;
+  elements.container.innerHTML = '';
+  textInput = evt.currentTarget.elements.searchQuery.value;
+  serviceImage(page, textInput)
+    .then(response => {
+      if (response.total === 0) {
+        Notiflix.Notify.failure('Sorry, there are no images. Please try again');
+      }
+
+      const el = response.hits;
+      return createMurcup(response);
+    })
+    .catch(err => console.log(err));
+}
+
 function downloadMore() {
   page += 1;
   serviceImage(page, textInput)
@@ -29,22 +46,6 @@ function downloadMore() {
         elements.btnLoad.classList.add('load-more-hidden');
         Notiflix.Notify.info('Sorry, its last page ):');
       }
-    })
-    .catch(err => console.log(err));
-}
-
-function onSubmit(evt) {
-  evt.preventDefault();
-  elements.container.innerHTML = '';
-  textInput = evt.currentTarget.elements.searchQuery.value;
-  serviceImage(1, textInput)
-    .then(response => {
-      if (response.total === 0) {
-        Notiflix.Notify.failure('Sorry, there are no images. Please try again');
-      }
-
-      const el = response.hits;
-      return createMurcup(response);
     })
     .catch(err => console.log(err));
 }
